5b73107a68c8792476d5e4a847ba31b621f8f667
Rafaéla Jàneczko <sagethirteen@gmail.com>
2025-05-16 19:38:47 -0300

    feat: adjust main file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication, Logger } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { AllExceptionsFilter } from './middlewares/error-handling.middleware';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+class App {
+  app: INestApplication;
+  swaggerConfig: Omit<OpenAPIObject, 'paths'>;
+
+  constructor() {
+    this.start();
+  }
+
+  async start() {
+    try {
+      await this.bootstrap();
+    } catch (error) {
+      Logger.fatal(error.message);
+    }
+  }
+
+  async bootstrap() {
+    this.app = await NestFactory.create(AppModule);
+    await this.swagger();
+    this.app.useGlobalFilters(new AllExceptionsFilter());
+    await this.app.listen(3000);
+  }
+
+  async swagger() {
+    this.swaggerConfig = new DocumentBuilder()
+      .setTitle('Music Streaming Challenge')
+      .setDescription('API documentation for the Music Streaming Challenge')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(
+      this.app as any,
+      this.swaggerConfig,
+    );
+    SwaggerModule.setup('api/docs', this.app as any, document);
+  }
 }
-bootstrap();
+
+export default new App();
---
+++ backend/src/middlewares/error-handling.middleware.ts
@@ -0,0 +1,35 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest();
+
+    let status: HttpStatus;
+    let message: string;
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      message = exception.getResponse() as string;
+    } else {
+      status = HttpStatus.INTERNAL_SERVER_ERROR;
+      message = 'Internal server error';
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message,
+    });
+  }
+}
